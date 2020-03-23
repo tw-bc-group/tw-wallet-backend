c5d08cd1ec9bd6eb9b8f07aea74ac0be20ae7d44
Jin <jbjxu@thoughtworks.com>
2020-03-23 16:50:04 +0800

    feat: initiate of tw-wallet repository

---
+++ src/main/java/com/thoughtworks/wallet/TWWalletApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TWWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TWWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thoughtworks/wallet/TWWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TWWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
