ae0c025943675f89958c367ced7b853b98be0a70
yin <yin.shen@thoughtworks.com>
2020-03-31 00:00:36 +0800

    feat: add integration test for get transaction

---
+++ src/main/java/com/thoughtworks/wallet/config/JacksonConfig.java
@@ -0,0 +1,43 @@
+package com.thoughtworks.wallet.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper serializingObjectMapper() {
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+        ObjectMapper mapper = JsonMapper.builder() // or different mapper for other format
+                .addModule(new ParameterNamesModule())
+                .addModule(new Jdk8Module())
+                .addModule(javaTimeModule)
+                .build();
+        return mapper;
+    }
+}
---
+++ src/test/java/com/thoughtworks/wallet/integrate/TransactionMvcIntegrationTest.java
@@ -0,0 +1,85 @@
+package com.thoughtworks.wallet.integrate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.wallet.asset.model.Transaction;
+import com.thoughtworks.wallet.common.ResponseBean;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("uat")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TransactionMvcIntegrationTest {
+
+    private static final Logger log = LoggerFactory.getLogger(TransactionMvcIntegrationTest.class);
+
+    /**
+     * @LocalServerPort 提供了 @Value("${local.server.port}") 的代替
+     */
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @BeforeEach
+    public void setup() {
+        String url = String.format("http://localhost:%d/v1/transactions", port);
+        System.out.println(String.format("port is : [%d]", port));
+        try {
+            this.base = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+//        restTemplate.setMessageConverters(Arrays.asList(new SecureHttpMessageConverter()));
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        restTemplateBuilder.configure(restTemplate);
+        testRestTemplate = new TestRestTemplate(restTemplateBuilder);
+
+        testRestTemplate.getRestTemplate().setInterceptors(
+                Collections.singletonList((request, body, execution) -> {
+                    request.getHeaders().setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+                    return execution.execute(request, body);
+                }));
+    }
+
+    @Test
+    public void whenGetTransactionByHash_thenReturnTransaction() throws Exception {
+        String hash = "9eff6287e55ea56b2abcf8d84a1a151e8a00e0f482ea0ee0448fef9f5d3ebad4";
+        String url = this.base.toString() + "/" + hash;
+        final ResponseEntity<ResponseBean> response = testRestTemplate.getForEntity(url, ResponseBean.class);
+        final ResponseBean responseBean = response.getBody();
+
+        final Transaction transaction = mapper.convertValue(responseBean.getResult(), Transaction.class);
+        assertThat(transaction.getHash()).isEqualTo(hash);
+    }
+}
