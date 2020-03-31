5795f5708ab69cff97897e57aad40a70885bb69c
Jin <jbjxu@thoughtworks.com>
2020-03-31 18:07:44 +0800

    style: refactor to suitable code style

--- src/main/java/com/thoughtworks/wallet/config/ModelMapConfig.java
+++ src/main/java/com/thoughtworks/wallet/config/ModelMapConfig.java
@@ -1,29 +1,17 @@
 package com.thoughtworks.wallet.config;
 
-import org.jooq.DSLContext;
-import org.jooq.SQLDialect;
-import org.jooq.impl.DataSourceConnectionProvider;
-import org.jooq.impl.DefaultConfiguration;
-import org.jooq.impl.DefaultDSLContext;
-import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.NameTokenizers;
 import org.modelmapper.jooq.RecordValueReader;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class ModelMapConfig {
 
-    private ModelMapper modelMapper;
-
     @Bean
     public ModelMapper modelMapper() {
-        modelMapper = new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().addValueReader(new RecordValueReader());
         modelMapper.getConfiguration().setSourceNameTokenizer(NameTokenizers.UNDERSCORE);
         return modelMapper;
