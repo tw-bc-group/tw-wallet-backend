ca1a44959a4ec1ddc990bfcd2b4fa344f2a7fe59
Jin <jbjxu@thoughtworks.com>
2020-03-31 18:06:55 +0800

    refactor: change naming to snake cases format

--- src/main/java/com/thoughtworks/wallet/asset/response/TWPointBalanceResponse.java
+++ src/main/java/com/thoughtworks/wallet/asset/response/TWPointBalanceResponse.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.wallet.asset.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.thoughtworks.wallet.asset.model.TWPoint;
 import com.thoughtworks.wallet.util.CoinSerializer;
@@ -8,6 +10,7 @@ import lombok.Data;
 import java.math.BigDecimal;
 
 @Data
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class TWPointBalanceResponse {
     private String address;
 
