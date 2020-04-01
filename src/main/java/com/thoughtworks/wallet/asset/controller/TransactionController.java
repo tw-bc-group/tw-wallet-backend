906fed3ef4e02f0a4b70168e35b4cc9660b0639b
xudongdong <dongdong.xu1@thoughtworks.com>
2020-04-01 17:05:42 +0800

    feat: change get transactions router #10

--- src/main/java/com/thoughtworks/wallet/asset/controller/TransactionController.java
+++ src/main/java/com/thoughtworks/wallet/asset/controller/TransactionController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,7 +38,7 @@ public class TransactionController {
     }
 
     @ApiOperation(value = "查询交易历史")
-    @RequestMapping(value = "/transactions", method = RequestMethod.GET)
+    @GetMapping
     public List<Transaction> getTxHistory(@RequestParam(name = "from_addr") String fromAddr) {
         return transactionService.listByFromAddress(fromAddr);
     }
