78356b7a4d0cf0541aca819737ee99a4165458f3
Jin <jbjxu@thoughtworks.com>
2020-04-01 12:21:04 +0800

    fix: change checksum address to normal valid address, due to checksum address is unavailable in java

--- src/main/java/com/thoughtworks/wallet/asset/service/impl/QuorumServiceImpl.java
+++ src/main/java/com/thoughtworks/wallet/asset/service/impl/QuorumServiceImpl.java
@@ -9,8 +9,6 @@ import com.thoughtworks.wallet.asset.model.TWPoint;
 import com.thoughtworks.wallet.asset.response.TWPointBalanceResponse;
 import com.thoughtworks.wallet.asset.service.IBlockchainService;
 import lombok.extern.slf4j.Slf4j;
-import org.bouncycastle.jcajce.provider.digest.SHA3;
-import org.bouncycastle.util.encoders.Hex;
 import org.springframework.stereotype.Service;
 import org.web3j.contracts.eip20.generated.ERC20;
 import org.web3j.crypto.Credentials;
@@ -23,8 +21,6 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-
-import static java.lang.Integer.parseInt;
 
 @Slf4j
 @Service
@@ -41,7 +37,7 @@ public class QuorumServiceImpl implements IBlockchainService {
 
     @Override
     public TWPointBalanceResponse getTWPointBalanceBy(String address) {
-        boolean isAddress = isChecksumAddress(address);
+        boolean isAddress = isValidAddress(address);
         if (!isAddress) {
             throw new InvalidAddressErrorException(address);
         }
@@ -71,53 +67,9 @@ public class QuorumServiceImpl implements IBlockchainService {
         }
     }
 
-    public static boolean isChecksumAddress(String addr) {
-        //Print for testing purpose and more verbose output
-        log.info("Incoming Address " + addr);
-
-        // First we need to check the address has the value between 0-9a-fA-F
+    public static boolean isValidAddress(String addr) {
         String regex = "^0x[0-9a-fA-F]{40}$";
-        if (!addr.matches(regex)) {
-            return false;
-        }
 
-        //to fetch the part after 0x
-        String subAddr = addr.substring(2);
-        //Make it to original lower case address
-        String subAddrLower = subAddr.toLowerCase();
-
-        //Print for testing purpose and more verbose output
-        log.info("Fetched Original Address " + subAddrLower);
-
-        // if the previous step validates then we will test the checksum part
-
-        // Create a SHA3256 hash (Keccak-256)
-        SHA3.DigestSHA3 digestSHA3 = new SHA3.Digest256();
-        digestSHA3.update(subAddrLower.getBytes());
-        String digestMessage = Hex.toHexString(digestSHA3.digest());
-
-        //Print for testing purpose and more verbose output
-        log.info("Hex String " + digestMessage);
-
-        /* Check each letter is upper case or not
-         * if it is upper case then the corresponding binary position of the hashed address
-         * should be 1 i.e the message digest letter should be getter than 7
-         * as 7 is the last Hex digit which starts with 0 in binary
-         * rest of all 8 to f starts with 1
-         */
-
-        for (short i = 0; i < subAddr.length(); i++) {
-            if (subAddr.charAt(i) >= 65 && subAddr.charAt(i) <= 91) {
-
-                log.info("Position Upper " + (subAddr.charAt(i)));
-                log.info("Position digest " + (digestMessage.charAt(i)));
-
-                String ss = Character.toString(digestMessage.charAt(i));
-                if (!(parseInt(ss, 16) > 7)) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        return addr.matches(regex);
     }
 }
