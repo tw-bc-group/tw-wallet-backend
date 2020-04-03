cb9ebace7f578836c9b7931eff5d96918b4d5c3c
Weili Yi <wlyi@thoughtworks.com>
2020-04-03 09:21:32 +0800

    update ci

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,24 +1,39 @@
 pipeline {
   agent any
+  environment {
+    DB_USERNAME     = credentials('DB_USERNAME')
+    DB_PASSWORD = credentials('TWPOINT_CONTRACT_ADDRESS')
+    NODE1_PRIVATE_KEY = credentials('NODE1_PRIVATE_KEY')
+    RPC_URL = credentials('RPC_URL')
+    DOCKER_REG = "${DOCKER_REG}"
+    TW_WALLET_IMAGE = "${DOCKER_REG}/tw-wallet:build-${BUILD_NUMBER}"
+  }
   stages {
     stage('Build') {
-            steps {
-                sh 'make cache'
-                sh 'make build'
-            }
-        }
+      steps {
+        sh 'make cache'
+        sh 'make build'
+      }
+    }
 
     stage('Dockerize') {
-        environment {
-          DOCKER_REG = "${DOCKER_REG}"
-          IMAGE = "${DOCKER_REG}/tw-wallet:build-${BUILD_NUMBER}"
-        }
-        steps {
-            sh 'aws ecr get-login-password | docker login  -u AWS --password-stdin $DOCKER_REG'
-            sh 'make image tag=$IMAGE'
-            sh 'docker push $IMAGE'
-        }
+      steps {
+        sh 'aws ecr get-login-password | docker login  -u AWS --password-stdin $DOCKER_REG'
+        sh 'make image TAG=$TW_WALLET_IMAGE'
+        sh 'docker push $TW_WALLET_IMAGE'
+        sh '/usr/local/bin/kompose convert -c -f docker/docker-compose.yml'
+        sh 'cd docker; /usr/local/bin/docker-compose up -d'
+      }
+    }
+
+    stage('Deploy') {
+      steps {
+        sh '/usr/local/bin/kompose convert -c -f docker/docker-compose.yml'
+        // TODO: Remove this will real helm install
+        sh 'cd docker; /usr/local/bin/docker-compose up -d'
+      }
     }
   }
 }
+
 
