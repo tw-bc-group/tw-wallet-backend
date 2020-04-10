4151aa7463542d97a393316567ec1299fb37df3a
Jin <jbjxu@thoughtworks.com>
2020-04-10 14:47:02 +0800

    fix: change the definition way of controller to fix request method 'GET' not supported

--- src/main/java/com/thoughtworks/wallet/asset/controller/IdentityController.java
+++ src/main/java/com/thoughtworks/wallet/asset/controller/IdentityController.java
@@ -6,9 +6,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -19,7 +19,7 @@ import javax.validation.Valid;
 @RequestMapping(value = "/v1/identities")
 @Api(tags = "身份相关操作")
 public class IdentityController {
-    @PostMapping
+    @RequestMapping(method = RequestMethod.POST)
     @ApiOperation(value = "注册身份")
     public void identityRegistry(@Valid @RequestBody IdentityRegistryRequest request) {
         // TODO call blockchain identity registry smart contract
