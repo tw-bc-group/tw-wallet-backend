14bc0af522f29ffada802710d10365197c95ca27
Jin <jbjxu@thoughtworks.com>
2020-04-28 14:39:53 +0800

    refactor: add jsr303 to validate valid healthy status

--- src/main/java/com/thoughtworks/wallet/healthyVerifier/HealthVerificationRequest.java
+++ src/main/java/com/thoughtworks/wallet/healthyVerifier/HealthVerificationRequest.java
@@ -2,31 +2,26 @@ package com.thoughtworks.wallet.healthyVerifier;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.thoughtworks.wallet.healthyVerifier.exception.InvalidHealthyStatusException;
-import lombok.Value;
+import com.thoughtworks.wallet.healthyVerifier.validator.ValidHealthyStatus;
+import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotBlank;
-
-@Value
 public class HealthVerificationRequest {
     @Length(min = 11, max = 11, message = "Phone number should only have 11 digits.")
+    @Getter
     String phone;
 
-    @NotBlank
-    HealthyStatus status;
+    @ValidHealthyStatus
+    String status;
 
     @JsonCreator
     public HealthVerificationRequest(@JsonProperty("phone") String phone,
                                      @JsonProperty("status") String status) {
-        HealthyStatus healthyStatus;
-        try {
-            healthyStatus = HealthyStatus.valueOf(status.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            throw new InvalidHealthyStatusException(status);
-        }
-
         this.phone = phone;
-        this.status = healthyStatus;
+        this.status = status;
+    }
+
+    public HealthyStatus getStatus() {
+        return HealthyStatus.valueOf(status.toUpperCase());
     }
 }
