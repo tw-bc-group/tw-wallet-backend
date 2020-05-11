d8be227cbce886fc5fbc7db48ea086535f5a3055
Jin <jbjxu@thoughtworks.com>
2020-05-11 15:27:56 +0800

    chore: introduce HEALTH_VERIFICATION_CLAIM_ISSUER_PRIVATE_KEY as environment variable

--- tw-wallet-webapp/src/main/java/com/thoughtworks/wallet/healthyVerifier/model/HealthVerificationClaimContract.java
+++ tw-wallet-webapp/src/main/java/com/thoughtworks/wallet/healthyVerifier/model/HealthVerificationClaimContract.java
@@ -2,6 +2,7 @@ package com.thoughtworks.wallet.healthyVerifier.model;
 
 import com.thoughtworks.wallet.healthyVerifier.annotation.HealthVerificationClaimContractAddress;
 import com.thoughtworks.wallet.healthyVerifier.annotation.HealthVerificationClaimIssuerAddress;
+import com.thoughtworks.wallet.healthyVerifier.annotation.HealthVerificationClaimIssuerPrivateKey;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Component;
 public class HealthVerificationClaimContract {
     @HealthVerificationClaimIssuerAddress
     private String issuerAddress;
+
+    @HealthVerificationClaimIssuerPrivateKey
+    private String issuerPrivateKey;
 
     @HealthVerificationClaimContractAddress
     private String address;
