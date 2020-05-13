a7a23639edb9ffb0c73daf715f320cb1da15fcf4
Jin <jbjxu@thoughtworks.com>
2020-05-13 14:04:36 +0800

    refactor: remove useless exception

--- tw-wallet-common/src/main/java/com/thoughtworks/common/exception/ErrorCode.java
+++ tw-wallet-common/src/main/java/com/thoughtworks/common/exception/ErrorCode.java
@@ -18,8 +18,7 @@ public enum ErrorCode {
     HEALTH_VERIFICATION_NOT_FOUND(BAD_REQUEST, 400_00, "该用户暂无健康认证信息"),
     HEALTH_VERIFICATION_CREATE_FAILED(BAD_REQUEST, 500_00, "用户健康认证信息上链失败"),
     HEALTH_VERIFICATION_ALREADY_EXIST(BAD_REQUEST, 400_00, "该用户已创建健康认证信息"),
-    INSERT_INTO_DATABASE_ERROR(BAD_REQUEST, 400_00, "健康认证信息入库错误"),
-    INVALID_HEALTHY_STATUS(BAD_REQUEST, 400_00, "健康状态填写有误，只可为'healthy'和'unhealthy'。");
+    INSERT_INTO_DATABASE_ERROR(BAD_REQUEST, 400_00, "健康认证信息入库错误");
 
     @NonNull
     private HttpStatus status;
