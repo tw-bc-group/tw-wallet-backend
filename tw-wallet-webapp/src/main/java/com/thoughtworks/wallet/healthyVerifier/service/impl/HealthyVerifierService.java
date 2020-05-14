6e75d264e35c71ac76b94f4b046d9134b4a3e9d0
yin <yin.shen@thoughtworks.com>
2020-05-15 01:38:22 +0800

    fix: build error of HealthyService

--- tw-wallet-webapp/src/main/java/com/thoughtworks/wallet/healthyVerifier/service/impl/HealthyVerifierService.java
+++ tw-wallet-webapp/src/main/java/com/thoughtworks/wallet/healthyVerifier/service/impl/HealthyVerifierService.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.wallet.healthyVerifier.service.impl;
 
+import com.google.common.collect.ImmutableList;
 import com.thoughtworks.wallet.gen.tables.records.TblHealthyVerificationClaimRecord;
 import com.thoughtworks.wallet.healthyVerifier.dto.ChangeHealthVerificationRequest;
 import com.thoughtworks.wallet.healthyVerifier.dto.HealthVerificationRequest;
@@ -9,40 +10,60 @@ import com.thoughtworks.wallet.healthyVerifier.exception.HealthVerificationNotFo
 import com.thoughtworks.wallet.healthyVerifier.exception.InsertIntoDatabaseErrorException;
 import com.thoughtworks.wallet.healthyVerifier.model.HealthVerificationClaim;
 import com.thoughtworks.wallet.healthyVerifier.model.HealthVerificationClaimContract;
+import com.thoughtworks.wallet.healthyVerifier.model.HealthyCredential;
+import com.thoughtworks.wallet.healthyVerifier.model.HealthyStatus;
+import com.thoughtworks.wallet.healthyVerifier.model.HealthyStatusWrapper;
+import com.thoughtworks.wallet.healthyVerifier.model.Result;
 import com.thoughtworks.wallet.healthyVerifier.repository.HealthVerificationDAO;
 import com.thoughtworks.wallet.healthyVerifier.service.IHealthyVerifierService;
-import com.thoughtworks.wallet.healthyVerifier.utils.ClaimUtil;
+import com.thoughtworks.wallet.healthyVerifier.utils.ClaimIdUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.jooq.DSLContext;
 import org.modelmapper.ModelMapper;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Optional;
 
 import static com.thoughtworks.wallet.gen.Tables.TBL_HEALTHY_VERIFICATION_CLAIM;
+import static com.thoughtworks.wallet.healthyVerifier.model.Result.YES;
 
 @Slf4j
 @Service
 public class HealthyVerifierService implements IHealthyVerifierService {
-    private final DSLContext                      dslContext;
-    private final ClaimUtil                       claimUtil;
-    private final HealthyClaimContractService     healthyClaimContractService;
-    private final HealthVerificationClaimContract healthClaimContract;
-    private final HealthVerificationDAO           healthVerificationDAO;
-    private final ModelMapper                     modelMapper = new ModelMapper();
+    private final DSLContext dslContext;
+    private final ClaimIdUtil claimIdUtil;
+    private final HealthyClaimContractService healthyClaimContractService;
+    private final HealthVerificationClaimContract healthVerificationClaimContract;
+    private final SuspectedPatientService suspectedPatientService;
+    private final HealthVerificationDAO healthVerificationDAO;
+    private final ModelMapper modelMapper = new ModelMapper();
 
-    public HealthyVerifierService(DSLContext dslContext, ClaimUtil claimUtil, HealthyClaimContractService healthyClaimContractService, HealthVerificationClaimContract healthClaimContract, HealthVerificationDAO healthVerificationDAO) {
+    public final static String didSchema = "DID:TW:";
+    private final String version = "0.1";
+    private final float maxHealthyTemperature = 37.3F;
+    final ImmutableList<String> context = ImmutableList.of("https://blockchain.thoughtworks.cn/credentials/v1/");
+    final ImmutableList<String> credentialType = ImmutableList.of("HealthyCredential");
+
+    // TODO 目前假设 claim 5 mins 过期
+    Duration expiredDuration = Duration.ofMinutes(5);
+
+    public HealthyVerifierService(DSLContext dslContext, ClaimIdUtil claimIdUtil, HealthyClaimContractService healthyClaimContractService, HealthVerificationClaimContract healthVerificationClaimContract, SuspectedPatientService suspectedPatientService, HealthVerificationDAO healthVerificationDAO) {
         this.dslContext = dslContext;
-        this.claimUtil = claimUtil;
+        this.claimIdUtil = claimIdUtil;
         this.healthyClaimContractService = healthyClaimContractService;
-        this.healthClaimContract = healthClaimContract;
+        this.healthVerificationClaimContract = healthVerificationClaimContract;
+        this.suspectedPatientService = suspectedPatientService;
         this.healthVerificationDAO = healthVerificationDAO;
     }
 
     @Override
     public HealthVerificationResponse createHealthVerification(HealthVerificationRequest healthVerification) {
-        HealthVerificationClaim claim = claimUtil.generateHealthyVerificationClaim(healthVerification.getDid(), healthVerification.getPhone(), healthClaimContract.getIssuerAddress());
+        HealthVerificationClaim claim = generateHealthyVerificationClaim(healthVerification);
+        String issuerDid = generateIssuerDid();
 
         final int insertedNumber;
         try {
@@ -57,7 +78,7 @@ public class HealthyVerifierService implements IHealthyVerifierService {
             throw new InsertIntoDatabaseErrorException(healthVerification.getDid());
         }
 
-        healthyClaimContractService.createHealthVerification(healthClaimContract.getIssuerAddress(), claim.getId(), healthVerification.getDid(), claim.getIss());
+        healthyClaimContractService.createHealthVerification(healthVerificationClaimContract.getIssuerAddress(), claim.getId(), healthVerification.getDid(), issuerDid);
 
         return HealthVerificationResponse.of(
                 claim.getContext(),
@@ -73,25 +94,82 @@ public class HealthyVerifierService implements IHealthyVerifierService {
 
     @Override
     public HealthVerificationResponse getHealthVerification(String ownerId) {
-        TblHealthyVerificationClaimRecord tblHealthyVerificationClaimRecord = Optional.ofNullable(dslContext
+        TblHealthyVerificationClaimRecord tblHealthyVerificationClaimRecord = dslContext
                 .selectFrom(TBL_HEALTHY_VERIFICATION_CLAIM)
                 .where(TBL_HEALTHY_VERIFICATION_CLAIM.OWNER.equal(ownerId))
                 .orderBy(TBL_HEALTHY_VERIFICATION_CLAIM.IAT.desc())
-                .fetchOne()).orElseThrow(() -> new HealthVerificationNotFoundException(ownerId));
+                .fetchOne();
 
         HealthVerificationClaim claim = new HealthVerificationClaim(tblHealthyVerificationClaimRecord);
         return modelMapper.map(claim, HealthVerificationResponse.class);
     }
 
     @Override
-    public HealthVerificationResponse changeHealthVerification(ChangeHealthVerificationRequest changeHealthVCRequest) {
+    public HealthVerificationResponse changeHealthVerification(ChangeHealthVerificationRequest changeHealthVerificationRequest) {
         TblHealthyVerificationClaimRecord tblHealthyVerificationClaimRecord = Optional.ofNullable(dslContext
                 .selectFrom(TBL_HEALTHY_VERIFICATION_CLAIM)
-                .where(TBL_HEALTHY_VERIFICATION_CLAIM.OWNER.equal(changeHealthVCRequest.getOwnerId()))
-                .fetchOne()).orElseThrow(() -> new HealthVerificationNotFoundException(changeHealthVCRequest.getOwnerId()));
-        HealthVerificationClaim claim = claimUtil.changeHealthyVerificationClaim(tblHealthyVerificationClaimRecord, changeHealthVCRequest.getHealthyStatus().getStatus());
+                .where(TBL_HEALTHY_VERIFICATION_CLAIM.OWNER.equal(changeHealthVerificationRequest.getOwnerId()))
+                .fetchOne()).orElseThrow(() -> new HealthVerificationNotFoundException(changeHealthVerificationRequest.getOwnerId()));
+
+        HealthVerificationClaim claim = new HealthVerificationClaim(tblHealthyVerificationClaimRecord);
+        claim.getSub().getHealthyStatus().setVal(changeHealthVerificationRequest.getHealthyStatus().getStatus());
+        final Instant now = Instant.now();
+        final long expiredTime = now.plus(expiredDuration).getEpochSecond();
+        claim.setExp(expiredTime);
         healthVerificationDAO.updateHealthVerificationClaim(claim);
         return modelMapper.map(claim, HealthVerificationResponse.class);
 
+    }
+
+    private HealthVerificationClaim generateHealthyVerificationClaim(HealthVerificationRequest healthVerification) {
+        final String did = healthVerification.getDid();
+        final String phone = healthVerification.getPhone();
+        final float temperature = healthVerification.getTemperature();
+        final Result contact = Result.of(healthVerification.getContact());
+        final Result symptoms = Result.of(healthVerification.getSymptoms());
+
+        final String claimId = claimIdUtil.generateClaimId(did, version);
+        log.info("Claim Id of did:{} is {}.", did, claimId);
+        String issuerDid = generateIssuerDid();
+
+        final Instant now = Instant.now();
+        final long currentTime = now.getEpochSecond();
+        final long expiredTime = now.plus(expiredDuration).getEpochSecond();
+
+        final HealthyStatusWrapper healthyStatus = generateHealthyStatus(phone, temperature, symptoms);
+
+        return HealthVerificationClaim.builder()
+                .context(context)
+                .id(claimId)
+                .ver(version)
+                .iss(issuerDid)
+                .iat(currentTime)
+                .exp(expiredTime)
+                .typ(credentialType)
+                .sub(HealthyCredential.of(did, phone, temperature, contact, symptoms, healthyStatus))
+                .build();
+    }
+
+    @NotNull
+    private String generateIssuerDid() {
+        return didSchema + healthVerificationClaimContract.getIssuerAddress().substring(2);
+    }
+
+    public HealthyStatusWrapper generateHealthyStatus(String phone, float temperature, Result symptoms) {
+        //TODO: mock healthy status:
+        //   1. Temperature > 37.3 -> UNHEALTHY
+        //   2. Has symptoms -> UNHEALTHY
+        //   3. Phone number in suspected list -> UNHEALTHY
+        //   4. Otherwise -> HEALTHY
+        final boolean isSuspectedPatient = suspectedPatientService.isSuspectedPatient(phone);
+
+        final boolean isHighTemperature = Float.compare(temperature, maxHealthyTemperature) >= 0;
+
+        final boolean hasSymptoms = symptoms.equals(YES);
+
+        if(isHighTemperature || isSuspectedPatient || hasSymptoms){
+            return HealthyStatusWrapper.of(HealthyStatus.UNHEALTHY.getStatus());
+        }
+        return HealthyStatusWrapper.of(HealthyStatus.HEALTHY.getStatus());
     }
 }
---
+++ tw-wallet-webapp/src/main/java/com/thoughtworks/wallet/healthyVerifier/utils/ClaimIdUtil.java
@@ -0,0 +1,45 @@
+package com.thoughtworks.wallet.healthyVerifier.utils;
+
+import com.thoughtworks.wallet.healthyVerifier.crypto.Base58;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.web3j.crypto.Hash;
+
+import java.util.Arrays;
+import static com.thoughtworks.wallet.healthyVerifier.service.impl.HealthyVerifierService.didSchema;
+
+
+@Slf4j
+@Component
+@NoArgsConstructor
+public class ClaimIdUtil {
+
+    /**
+     * 参考生成规则：(https://github.com/ontio/ontology-DID/blob/master/docs/cn/ONTID_protocol_spec_cn.md)
+     * 1. 根据用户的地址，计算 h = sha256hash160(address），data = version || h；
+     * 2. 对 data 计算两次 sha256，并取结果哈希的前 4 字节作为校验，即 checksum = sha256(sha256(data))[0:4]；
+     * 3. 令 idString = base58(data || checksum)[0, 40]；
+     * 4. 将 "did:tw:" 与 idString 拼接，即 ID = "did:tw:" || idString；
+     * 5. 输出 ID。
+     * 上述过程中，|| 表示连接前后两个字节串，version 是当前版本。
+     *
+     * @param did
+     * @return claim id
+     * @throws Exception
+     */
+    public String generateClaimId(String did, String version) {
+        final String address = getAddressFromDid(did);
+        final byte[] h = Hash.sha256hash160(address.getBytes());
+        final String data = version.concat(Arrays.toString(h));
+        final String checkSum = Arrays.toString(Hash.sha256(Hash.sha256(data.getBytes()))).substring(0, 4);
+        final String idString = Base58.encode(checkSum.concat(data).getBytes()).substring(0, 40);
+
+        return didSchema.concat(idString);
+    }
+
+    String getAddressFromDid(String did) {
+        did = did.toLowerCase();
+        return did.replace("did:tw:", "0x");
+    }
+}
