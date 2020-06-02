7c074cae097694794a37fa33b6e4f4cb2fae28f7
yin <yin.shen@thoughtworks.com>
2020-06-02 16:09:03 +0800

    chore(ci): use docker hub

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,67 +1,71 @@
 def COLOR_MAP = [
-    'SUCCESS': 'good', 
-    'FAILURE': 'danger',
+        'SUCCESS': 'good',
+        'FAILURE': 'danger',
 ]
 
 pipeline {
-  agent any
-  environment {
-    DB_USERNAME     = "${DB_USERNAME}"
-    DB_PASSWORD = "${DB_PASSWORD}"
-    DB_URL = "${DB_URL}"
-    NODE1_PRIVATE_KEY = "${NODE1_PRIVATE_KEY}"
-    TWPOINT_CONTRACT_ADDRESS = "${TWPOINT_CONTRACT_ADDRESS}"
-    IDENTITY_REGISTRY_CONTRACT_ADDRESS = "${IDENTITY_REGISTRY_CONTRACT_ADDRESS}"
-    HEALTH_VERIFICATION_CLAIM_CONTRACT_ADDRESS = "${HEALTH_VERIFICATION_CLAIM_CONTRACT_ADDRESS}"
-    HEALTH_VERIFICATION_CLAIM_ISSUER_ADDRESS = "${HEALTH_VERIFICATION_CLAIM_ISSUER_ADDRESS}"
-    HEALTH_VERIFICATION_CLAIM_ISSUER_PRIVATE_KEY = "${HEALTH_VERIFICATION_CLAIM_ISSUER_PRIVATE_KEY}"
-    RPC_URL = "${RPC_URL}"
-    DOCKER_REG = "${DOCKER_REG}"
-    LOG_DIR = "${LOG_DIR}"
-    HOST_IP = "${HOST_IP}"
-    TW_WALLET_IMAGE = "${DOCKER_REG}/tw-wallet:build-${BUILD_NUMBER}"
-  }
-  stages {
-    stage('Migration') {
-      steps {
-        sh './gradlew --no-daemon -Penv=dev flywayMigrate'
-      }
+    agent any
+    environment {
+        DB_USERNAME = "${DB_USERNAME}"
+        DB_PASSWORD = "${DB_PASSWORD}"
+        DB_URL = "${DB_URL}"
+        NODE1_PRIVATE_KEY = "${NODE1_PRIVATE_KEY}"
+        TWPOINT_CONTRACT_ADDRESS = "${TWPOINT_CONTRACT_ADDRESS}"
+        IDENTITY_REGISTRY_CONTRACT_ADDRESS = "${IDENTITY_REGISTRY_CONTRACT_ADDRESS}"
+        HEALTH_VERIFICATION_CLAIM_CONTRACT_ADDRESS = "${HEALTH_VERIFICATION_CLAIM_CONTRACT_ADDRESS}"
+        HEALTH_VERIFICATION_CLAIM_ISSUER_ADDRESS = "${HEALTH_VERIFICATION_CLAIM_ISSUER_ADDRESS}"
+        HEALTH_VERIFICATION_CLAIM_ISSUER_PRIVATE_KEY = "${HEALTH_VERIFICATION_CLAIM_ISSUER_PRIVATE_KEY}"
+        RPC_URL = "${RPC_URL}"
+        DOCKER_REG = "thoughtwallet"
+        LOG_DIR = "${LOG_DIR}"
+        HOST_IP = "${HOST_IP}"
+        TW_WALLET_IMAGE = "${DOCKER_REG}/tw-wallet:build-${BUILD_NUMBER}"
+    }
+    stages {
+        stage('Migration') {
+            steps {
+                sh './gradlew --no-daemon -Penv=dev flywayMigrate'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                // sh 'make cache'
+                // sh 'make build'
+                // TODO: Remove this workaround
+                sh './gradlew --no-daemon -Penv=dev clean build -x integTest'
+            }
+        }
+
+        stage('Dockerize') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'wallet-docker-account', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USER')]) {
+                    sh '''
+                    echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER --password-stdin
+                    '''
+                }
+                sh 'make image TAG=$TW_WALLET_IMAGE'
+                sh 'docker push $TW_WALLET_IMAGE'
+                sh 'docker rmi $TW_WALLET_IMAGE'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                sh 'env > .env'
+                sh 'make deploy.sync TAG=$TW_WALLET_IMAGE'
+                sh 'make deploy.webapp TAG=$TW_WALLET_IMAGE'
+                sh 'make deploy.webapp.service'
+            }
+        }
     }
 
-    stage('Build') {
-      steps {
-        // sh 'make cache'
-        // sh 'make build'
-        // TODO: Remove this workaround
-        sh './gradlew --no-daemon -Penv=dev clean build -x integTest'
-      }
+    post {
+        always {
+            echo 'Time to send slack message.'
+            slackSend channel: '#wallet',
+                    color: COLOR_MAP[currentBuild.currentResult],
+                    message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} on (<${env.BUILD_URL}|Open>)"
+        }
     }
-
-    stage('Dockerize') {
-      steps {
-        sh 'aws ecr get-login-password | docker login  -u AWS --password-stdin $DOCKER_REG'
-        sh 'make image TAG=$TW_WALLET_IMAGE'
-        sh 'docker push $TW_WALLET_IMAGE'
-        sh 'docker rmi $TW_WALLET_IMAGE'
-      }
-    }
-
-    stage('Deploy') {
-      steps {
-        sh 'env > .env'
-        sh 'make deploy.sync TAG=$TW_WALLET_IMAGE'
-        sh 'make deploy.webapp TAG=$TW_WALLET_IMAGE'
-        sh 'make deploy.webapp.service'
-      }
-    }
-  }
-
-  post {
-    always {
-        echo 'Time to send slack message.'
-        slackSend channel: '#wallet',
-            color: COLOR_MAP[currentBuild.currentResult],
-            message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} on (<${env.BUILD_URL}|Open>)"
-    }
-  }
 }
