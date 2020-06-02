497d7c38b58575de65a7bb44dc2282f732f0c281
Weili Yi <wlyi@thoughtworks.com>
2020-06-02 17:34:00 +0800

    Revert "chore(ci): push images to bc docker reg"
    This reverts commit 99adeb7107af9137f84d5b3eaec2fc22d9d700d6.

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,10 +16,10 @@ pipeline {
         HEALTH_VERIFICATION_CLAIM_ISSUER_ADDRESS = "${HEALTH_VERIFICATION_CLAIM_ISSUER_ADDRESS}"
         HEALTH_VERIFICATION_CLAIM_ISSUER_PRIVATE_KEY = "${HEALTH_VERIFICATION_CLAIM_ISSUER_PRIVATE_KEY}"
         RPC_URL = "${RPC_URL}"
-        DOCKER_REG = "${BC_DOCKER_REG}"
+        DOCKER_REG = "thoughtwallet"
         LOG_DIR = "${LOG_DIR}"
         HOST_IP = "${HOST_IP}"
-        TW_WALLET_IMAGE = "${BC_DOCKER_REG}/tw-wallet:build-${BUILD_NUMBER}"
+        TW_WALLET_IMAGE = "${DOCKER_REG}/tw-wallet:build-${BUILD_NUMBER}"
     }
     stages {
         stage('Migration') {
@@ -39,6 +39,11 @@ pipeline {
 
         stage('Dockerize') {
             steps {
+                withCredentials([usernamePassword(credentialsId: 'wallet-docker-account', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USER')]) {
+                    sh '''
+                    echo $DOCKER_PASSWORD | docker login --username $DOCKER_USER --password-stdin
+                    '''
+                }
                 sh 'make image TAG=$TW_WALLET_IMAGE'
                 sh 'docker push $TW_WALLET_IMAGE'
                 sh 'docker rmi $TW_WALLET_IMAGE'
