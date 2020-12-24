bf0d22c0e2ae3ae6f7263c540532f342969354ed
yizhu <chenzhicheng1994@gmail.com>
2020-12-24 09:41:01 +0800

    fix: fix issuers and vc_types sql

--- tw-wallet-webapp/src/main/resources/migration/V1.13__create_tbl_vc_types.sql
+++ tw-wallet-webapp/src/main/resources/migration/V1.13__create_tbl_vc_types.sql
@@ -1,10 +1,12 @@
 DROP TABLE IF EXISTS tbl_vc_types;
 create table tbl_vc_types
 (
-    id      serial          not null primary key,
-    name    varchar(255)    not null,
-    issuer  int             not null,
+    id      varchar(128)    not null,
+    name    varchar(128)    not null,
+    issuer  varchar(128)    not null,
     content text[]          not null,
 
+    constraint tbl_vc_types_pkey
+                primary key(id),
     FOREIGN KEY(issuer) REFERENCES tbl_issuers(id) on delete cascade on update cascade
 );
--- tw-wallet-webapp/src/main/resources/migration/v1.12__create_tbl_issuers.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS tbl_issuers;
-create table tbl_issuers
-(
-    id      serial          not null primary key,
-    name    varchar(255)    not null
-);
