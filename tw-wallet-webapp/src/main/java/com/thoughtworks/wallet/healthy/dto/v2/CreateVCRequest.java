b8d1a45a52d362b757b169842b620ba9a64db7a6
yin <yin.shen@thoughtworks.com>
2021-01-06 15:24:20 +0800

    feat(43): AC3 输入很长的字符报的信息不对

--- tw-wallet-webapp/src/main/java/com/thoughtworks/wallet/healthy/dto/v2/CreateVCRequest.java
+++ tw-wallet-webapp/src/main/java/com/thoughtworks/wallet/healthy/dto/v2/CreateVCRequest.java
@@ -2,7 +2,7 @@ package com.thoughtworks.wallet.healthy.dto.v2;
 
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 
 /**
  申请VC的API
@@ -11,13 +11,16 @@ import javax.validation.constraints.NotBlank;
 public class CreateVCRequest {
     @NotBlank
     @Getter
+    @Pattern(regexp = "^DID:TW:.{1,255}")
     String did;
 
     @NotBlank
     @Getter
+    @Size(min=5, max=255,message = "The length of phone is  between 3 and 255")
     String phone;
 
     @NotBlank
     @Getter
+    @Size(min=3, max=255,message = "The length of name is between 3 and 255")
     String name;
 }
