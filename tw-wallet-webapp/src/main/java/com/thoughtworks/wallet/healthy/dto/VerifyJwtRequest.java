92850ef2007aaef9fd8d837028e166fb723852b8
yizhu <chenzhicheng1994@gmail.com>
2021-01-11 15:02:24 +0800

    feat: add verifier did when verify TravelBadge

--- tw-wallet-webapp/src/main/java/com/thoughtworks/wallet/healthy/dto/VerifyJwtRequest.java
+++ tw-wallet-webapp/src/main/java/com/thoughtworks/wallet/healthy/dto/VerifyJwtRequest.java
@@ -3,9 +3,14 @@ package com.thoughtworks.wallet.healthy.dto;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 public class VerifyJwtRequest {
+    @NotBlank
+    @Getter
+    @Size(min = 5, max = 100, message = "The length of id is between 5 and 100")
+    String id;
     @NotBlank
     @Getter
     String token;
